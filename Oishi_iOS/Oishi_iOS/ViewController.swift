5f3e62b9d685abc01dd8b28f3b482050d22afd55
은호 <127753071+Eunho0922@users.noreply.github.com>
2024-08-01 17:30:32 +0900

    프로젝트 생성

---
+++ Oishi_iOS/Oishi_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Oishi_iOS
+//
+//  Created by 이은호 on 8/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
